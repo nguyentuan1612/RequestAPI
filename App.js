7dee43f343fe5f310e3157806609164f045da5a7
Nguyen Tuan <tuanbeo203@gmail.com>
2022-12-25 00:28:48 +0700

    new changes

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, Text, View, Image } from "react-native";
+import Home from "./components/home";
 
 export default App = () => {
   const [isLoading, setLoading] = useState(true);
@@ -22,17 +23,15 @@ export default App = () => {
   }, []);
 
   return (
-    <View style={{ flex: 1, padding: 24 }}>
+    <View style={{ flex: 1, padding: 5 }}>
       {isLoading ? (
         <ActivityIndicator size="large" style={{ flex: 1 }} />
       ) : (
         <FlatList
+        showsVerticalScrollIndicator={false}
+          numColumns = {2}
           data={data}
-          renderItem={({ item }) => (
-            <Text>
-              {item.rating.rate}, {item.rating.count}
-            </Text>
-          )}
+          renderItem={({ item }) => <Home image={item.image} title ={item.title} price ={item.price}  rating = {item.rating.rate}/>}
         />
       )}
     </View>
--- components/home.js
+++ components/home.js
@@ -1,13 +1,60 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Rating, AirbnbRating } from 'react-native-ratings';
+import { View, Text, StyleSheet, Image } from "react-native";
+import React from "react";
+import { Rating, AirbnbRating } from "react-native-ratings";
 
-const Home = () => {
+const Home = (Props) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image
+        source={{ uri: Props.image }}
+        style={{
+          width: 130,
+          height: 130,
+          resizeMode: "contain",
+          marginTop: 15,
+        }}
+      />
+      <Text style={styles.title} numberOfLines={1}>
+        {Props.title}
+      </Text>
+      <View
+        style={{
+          width: "86%",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginTop: 20,
+          borderWidth: 1,
+          height: 30,
+        }}
+      >
+        <Text>{Props.price}</Text>
+        <Rating
+          ratingCount={5}
+          imageSize={13}
+          readonly= {true}
+          startingValue={Props.rating}
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+const styles = StyleSheet.create({
+  container: {
+    width: 180,
+    height: 250,
+    borderWidth: 1,
+    marginRight: 16,
+    marginLeft: 8,
+    marginVertical: 8,
+    alignItems: "center",
+    padding: 6,
+  },
+  title: {
+    fontWeight: "bold",
+    marginTop: 20,
+  },
+});
+
+export default Home;
