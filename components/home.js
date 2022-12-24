b5bb49590bc395901688ece7ff18d6d92bb4c9a5
Nguyen Tuan <tuanbeo203@gmail.com>
2022-12-24 23:55:04 +0700

    new changes

---
+++ components/home.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Rating, AirbnbRating } from 'react-native-ratings';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
